58be72ca721dd9e4d4509fd65b942a3088bc052c
ramirezelena <elegisslle04@gmail.com>
2026-08-28 10:11:04 -0300

    test: agregar pruebas de registro de usuarios

--- usuarios/tests.py
+++ usuarios/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class RegistroUsuarioTests(TestCase):
+    def test_ruta_registro_existe(self):
+        response = self.client.get(reverse("usuarios:registro"))
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_registro_redirige_a_keycloak(self):
+        response = self.client.get(reverse("usuarios:registro"))
+
+        self.assertIn(
+            "http://localhost:8080/realms/global-exchange/"
+            "protocol/openid-connect/registrations",
+            response.url,
+        )
+
+    def test_registro_usa_cliente_correcto(self):
+        response = self.client.get(reverse("usuarios:registro"))
+
+        self.assertIn(
+            "client_id=global-exchange-web",
+            response.url,
+        )
